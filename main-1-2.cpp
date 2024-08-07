c4e0e01223c0680fb0ed08fb67d5bae5abfe9945
MikeHE-CODER <a1918791@adelaide.edu.au>
2024-08-07 13:34:11 +0930

    Update main-1-2.cpp

--- main-1-2.cpp
+++ main-1-2.cpp
@@ -1,6 +1,6 @@
 #include <iostream>
 extern void modifyArray(double* array, int size, double value);
 int main(){
-    double array={1,2,3,4,5};
+    double array[5]={1,2,3,4,5};
     modifyArray(array,5,3);
 }
