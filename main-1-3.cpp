f1b0e01e8c00e336a7a57fe3d70ce29560ca3321
MikeHE-CODER <a1918791@adelaide.edu.au>
2024-08-07 13:34:22 +0930

    Update main-1-3.cpp

--- main-1-3.cpp
+++ main-1-3.cpp
@@ -1,6 +1,6 @@
 #include <iostream>
 extern double* duplicateArray(double* array, int size);
 int main(){
-    double array={1,2,3,4,5};
+   double array[5]={1,2,3,4,5};
     double* newarray=duplicateArray(array,5);
 }
